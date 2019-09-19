76d87dc95258eccccf7824bbfdca96fffdf67ed0
Joey <jobbowen@iu.edu>
2019-09-19 14:34:18 -0400

    Update
    update

--- InClass/9-19-19/app/app.js
+++ InClass/9-19-19/app/app.js
@@ -1,19 +1,19 @@
 var theplace = document.querySelector("#theplace");
 var cardTemplate = document.querySelector("#cardTemplate");
 
-var suits = ["🍷", "🎱", "🌆", "🎁"];
-var values = ["A", "1", "2", "3"];
+var suits = [ "🍷", "🎱", "🌆", "🎁" ];
+var values = [ "A", "1", "2", "3" ];
 
 var i = 0;
-suits.forEach((suit) => {
-    values.forEach((value) => {
+suits.forEach( (suit) => {
+    values.forEach( (value) => {
         //We know we have a specific combination of suit + value
 
         //create and customize the element
         let newCard = document.createElement("div");
         newCard.innerHTML = value + " " + suit;
         newCard.classList.add("card");
-        newCard.style.animationDelay = i * .1 + "s";
+        newCard.style.animationDelay = i*.1 + "s";
 
 
         //set up connections and events
@@ -26,12 +26,15 @@ suits.forEach((suit) => {
         i++;
 
     })
-});
+})
 
 function onCardOver(event) {
+    event.target.style.animationDelay = "0s";
     event.target.classList.add("cardOver");
+    event.target.classList.remove("cardOut");
 }
 
 function onCardOut(event) {
-
+    event.target.classList.add("cardOut");
+    event.target.classList.remove("cardOver");
 }
