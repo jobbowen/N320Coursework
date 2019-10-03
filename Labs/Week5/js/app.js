464a30dbb064d1f1b7a0c4f306542c8599eaf504
Joey <jobbowen@iu.edu>
2019-10-03 17:48:38 -0400

    Update
    update

--- Labs/Week5/js/app.js
+++ Labs/Week5/js/app.js
@@ -1,30 +1,92 @@
 Vue.component("student-card", {
-    props: ["student", "isActive"],
-    template: "<div class='student' v-bind:class='{cardActive:isActive}'>{{student.name}} : {{student.skill}}</div>"
+    props: [ "student", "isactive" ],
+    template: "<div class='student' v-bind:class='{ cardActive:isactive, cardOut:!isactive }'>{{student.name}} : {{student.skill}}</div>"
 });
 
-let app = new Vue({
+Vue.component("student-card-two", {
+    props: [ "student", "isactivereverse" ],
+    template: "<div class='student' v-bind:class='{ cardActiveReverse:isactivereverse, cardOutReverse:!isactivereverse }'>{{student.name}} : {{student.skill}}</div>"
+});
+
+var app = new Vue({
     el: "#app",
     data: {
         students: [
-            {name: "Sienna", skill: 2, joy: 0},
-            {name: "Cyan", skill: 0, joy: 5},
-            {name: "Magenta", skill: 3, joy: 3}
+            { name: "Sienna", skill: 2, joy: 0 },
+            { name: "Cyan", skill: 0, joy: 5 },
+            { name: "Magenta", skill: 3, joy: 3 }
         ],
-        currentStudent: {name: "Sienna", skill: 2, joy: 0},
-        currentStudentId: 0,
-        cardActive: true
+        counter: 0,
+        currentStudent: { name: "Sienna", skill: 2, joy: 0 },
+        curStudentId: 0,
+        cardActive: true,
+        cardActiveReverse: false
     },
     methods: {
-        arrowClicked: function () {
-            this.currentStudentId++;
-            this.currentStudent = this.students[this.currentStudentId];
-
-            if (this.currentStudentId >= this.students.length - 1) {
-                this.currentStudentId = -1;
-            }
+        arrowClicked: function() {
 
             this.cardActive = !this.cardActive;
+
+            setTimeout( () => {
+                //modify the skill of the current student
+                //before moving onward:
+                this.currentStudent.skill ++;
+
+                //iteration code
+                this.curStudentId ++;
+                this.currentStudent = this.students[this.curStudentId];
+
+                if(this.curStudentId >= this.students.length-1) {
+                    this.curStudentId = -1;
+                }
+
+                //animation trigger
+                this.cardActive = !this.cardActive;
+
+            }, 300);
+        },
+
+        arrowClickedReverse: function() {
+
+            this.cardActive = !this.cardActive;
+
+            setTimeout( () => {
+                //modify the skill of the current student
+                //before moving onward:
+                this.currentStudent.skill --;
+
+                //iteration code
+                this.curStudentId ++;
+                this.currentStudent = this.students[this.curStudentId];
+
+                if(this.curStudentId >= this.students.length-1) {
+                    this.curStudentId = -1;
+                }
+
+                //animation trigger
+                this.cardActiveReverse = !this.cardActiveReverse;
+                this.cardActive = !this.cardActive;
+
+            }, 300);
+        },
+
+        increment() {
+            this.counter++;
         }
+
     }
 });
+
+
+/*
+setTimeout( () => {
+          this.currentStudent.skill ++;
+
+          this.curStudentId ++;
+          if(this.curStudentId >= this.students.length) this.curStudentId = 0;
+          this.currentStudent = this.students[this.curStudentId];
+          this.cardActive = !this.cardActive;
+      },
+      300)
+      cardOut:!isactive
+      */
